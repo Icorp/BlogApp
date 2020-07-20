65e5966ec58b87962924243005426f79b520361d
Icorp <itteam1602@gmail.com>
2020-07-20 11:50:34 +0600

    change theme, name project and starting server

--- main.go
+++ main.go
@@ -1,6 +1,28 @@
 package main
-import "fmt"
+import (
+	"fmt"
+	"github.com/gorilla/mux"
+	"html/template"
+	"net/http"
+)
 
-func main(){
-	fmt.Println("Hello")
+func productsHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+	response := fmt.Sprintf("Product %s", id)
+	fmt.Fprint(w, response)
+}
+
+func main() {
+
+	router := mux.NewRouter()
+	router.HandleFunc("/products/{id:[0-9]+}", productsHandler)
+	router.HandleFunc("/",IndexPage)
+
+	fmt.Println("Server is listening...")
+	http.ListenAndServe(":8181", router)
+}
+func IndexPage(w http.ResponseWriter, r *http.Request)  {
+	tmpl,_:=template.ParseFiles("wwwroot/index.html")
+	tmpl.Execute(w,"")
 }
